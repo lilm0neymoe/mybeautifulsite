53b52b8ff9ff19b198408da67668209f7c4b24b7
LilM0neyMoe <saitama07110@gmail.com>
2025-05-23 16:00:29 +0200

    fixed the real issue

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 // auth konstants
 const CLIENT_ID = "5ae6a0ba7af34db8b236a21abf9a83e6";
-const REDIRECT_URI = "http://127.0.0.1:5501/set_username.html";
+const REDIRECT_URI = "https://lilm0neymoe.github.io/mybeautifulsite/set_username.html";
 const AUTH_URL = "https://accounts.spotify.com/authorize";
 const SCOPES = "user-read-private user-top-read user-read-recently-played";
 
